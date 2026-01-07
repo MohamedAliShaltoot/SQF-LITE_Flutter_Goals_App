39e9331c819b7192dfef59356a99e715e8435f0a
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-01-07 19:26:16 +0200

    add functionality to add floating action button

--- lib/layouts/main_layout/main_layout.dart
+++ lib/layouts/main_layout/main_layout.dart
@@ -42,7 +42,70 @@ class MainLayout extends StatelessWidget {
           backgroundColor: Colors.teal,
           shape: CircleBorder(side: BorderSide(color: Colors.teal, width: 2)),
           onPressed: () {
-            // Action when button is pressed
+            showDialog(
+              context: context,
+              builder: (_) {
+                return AlertDialog(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  title: Text(AppStrings.addNewGoal),
+                  content: TextFormField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.grey[200],
+                    
+                      // floatingLabelBehavior: FloatingLabelBehavior.never,
+                      //label: Text(AppStrings.enterGoalHint),
+                      labelText: AppStrings.enterGoalHint,
+                      labelStyle: TextStyle(color: Colors.teal),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.grey),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: BorderSide(color: Colors.teal),
+                      ),
+                    ),
+                  ),
+                  actions: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        AppStrings.cancel,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.teal,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      onPressed: () {
+                        // Logic to add the goal goes here
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(
+                        AppStrings.add,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            );
           },
           child: const Icon(Icons.add, size: 30, color: Colors.white),
         ),
