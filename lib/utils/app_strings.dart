00ed6a5b0dc302a01f28838b30a42c519f7b29df
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-01-07 19:26:37 +0200

    just small refactor

--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -1,3 +1,7 @@
 abstract class AppStrings {
   static const String appTitle = 'GOALS';
+  static const String addNewGoal = 'Add New Goal';
+  static const String enterGoalHint = 'Enter your goal here';
+  static const String cancel = 'Cancel';
+  static const String add = 'Add';
 }
